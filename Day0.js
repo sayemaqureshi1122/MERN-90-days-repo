cf8f68e98c31fb5aced64b014e67ee4480ec7907
Sayema Qureshi <qureshisayema47@gmail.com>
2025-11-19 19:36:31 +0530

    ques solved

--- Day0.js
+++ Day0.js
@@ -23,3 +23,34 @@ function b(){
     var x = 100;
     console.log(x)
 }
+
+// Q1] write a js program to print the marks of students
+
+let marks = {
+    sayema :90,
+    vidu: 89,
+    bux : 17
+};
+
+for (let i = 0; i <Object.keys(marks).length; i++){
+    console.log(Object.keys(marks)[i]+ "'s marks are " + marks[Object.keys(marks)[i]]);
+}
+let num = 10;
+//Q2] Write a function declaration named square that takes a number and returns its square.
+const square = (num) =>{
+    return num*num;
+}
+square_res = square(num);
+console.log(square_res);
+
+//Q3] Write a function declaration isEven that returns true if a number is even, otherwise false.
+const is_even = (num) =>{
+    if(num % 2 == 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+let isnumeven = is_even(num);
+console.log(isnumeven);
