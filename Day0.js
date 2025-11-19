3cdf811627126702ed3d809f9b8bfc1f51874d84
Sayema Qureshi <qureshisayema47@gmail.com>
2025-11-19 19:07:59 +0530

    cocept and syntax

--- Day0.js
+++ Day0.js
@@ -1,1 +1,25 @@
 console.log("Day 0 discipline start");
+// Topics to cover today
+/*
+Minimum 25 lines demonstrating:
+
+Declaration vs expression vs arrow
+Function returning values
+Nested function
+Parameters vs arguments
+Function execution context difference
+*/
+var x = 1;
+
+a();
+b();
+console.log(x);
+function a(){
+    var x = 10;
+    console.log(x);
+}
+
+function b(){
+    var x = 100;
+    console.log(x)
+}
